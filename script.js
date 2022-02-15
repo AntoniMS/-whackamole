b35079e77558a07ebf10a1762d1445a04a683985
Toni M <tonims.dev@gmail.com>
2022-02-15 21:57:37 +0100

    Update

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-const mole = document.body.querySelectorAll('.mole');
-const hole = document.body.querySelectorAll('.hole');
-const score = document.body.querySelector('.score');
+const moles = document.body.querySelectorAll(".mole");
+const holes = document.body.querySelectorAll(".hole");
+const scoreBoard = document.body.querySelector(".score");
 
+let timeUp = false;
+let score = 0;
+let lastHole;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(250, 950);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+
+function whack(smash) {
+  if (!smash.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+moles.forEach((mole) => mole.addEventListener("click", whack));
